4f45452bee2561c7215a13baac2fe37308d4bbbb
Megan Yang <myang64@berkeley.edu>
2021-05-14 20:59:52 -0700

    add 2021 year filter

--- models/example/daily_count.sql
+++ models/example/daily_count.sql
@@ -5,5 +5,6 @@ SELECT
     EXTRACT(DAYOFWEEK FROM BLOCK_TIMESTAMP) as day,
     COUNT(DISTINCT from_address) AS active_addresses,
 FROM `public-data-finance.crypto_polygon.transactions`
+WHERE EXTRACT(YEAR FROM BLOCK_TIMESTAMP) = 2021
 GROUP BY day
 ORDER BY day ASC
