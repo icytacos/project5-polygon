e045a1871d01190d2a0dde05cbebc6a4f97e336c
Niraek Jain-Sharma <niraekjs@berkeley.edu>
2021-05-14 22:15:09 -0700

    added some nonces

--- models/example/weekday_counts.sql
+++ models/example/weekday_counts.sql
@@ -6,7 +6,7 @@ FROM(
     SELECT 
         DISTINCT EXTRACT(DAY FROM BLOCK_TIMESTAMP) AS day,
         EXTRACT(DAYOFWEEK FROM BLOCK_TIMESTAMP) AS weekday
-    FROM `public-data-finance.crypto_polygon.transactions`
+    FROM {{ref('filter_nonce')}}
 ) AS temp 
 GROUP BY weekday
 ORDER BY weekday ASC
